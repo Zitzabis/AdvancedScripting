b26c2128e6944a041931541152623bc4a45dde1f
Stephen Floyd <zitzabis.zitz@gmail.com>
2017-09-19 13:49:49 -0400

    JavaScript: Adds Jquery.

--- php/javascript.php
+++ php/javascript.php
@@ -2,4 +2,5 @@
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="js/bootstrap.js"></script>
+<script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+<script src="../../js/bootstrap.js"></script>
