d66122e6b554fedcdfce9409295c9b4c061e1994
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2017-10-05 00:56:05 -0400

    Project 4: Add article form.

---
+++ projects/project4/add_article.php
@@ -0,0 +1,39 @@
+<?php include("../../php/html_head.php") ?>
+
+  <body>
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills float-right">
+            <li class="nav-item">
+              <a class="nav-link active" href="../../index.php">Index <span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">Project #4 "Designing the Database"</h3>
+      </div>
+
+      <h5 class="text-muted">Add Article</h5>
+
+      <form class="form text-center" action="scripts/article_add.php">
+        <input type="hidden" class="form-control" id="row" name="row" value="null">
+        <div class="form-group" style="padding: 0.5em;">
+          <input type="text" class="form-control" id="title" name="title" placeholder="Title">
+        </div>
+        <div class="form-group" style="padding: 0.5em;">
+          <textarea class="form-control" id="body" name="body"></textarea>
+        </div>
+        <button type="submit" class="btn btn-success">Post Article</button>
+      </form>
+
+      <br>
+
+      <footer class="footer">
+        <p>&copy; Stephen Floyd <?php echo date("Y"); ?></p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <?php include("../../php/javascript.php") ?>
+  </body>
+</html>
