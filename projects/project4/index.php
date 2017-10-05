1a478b5bd06b4adf3caf2f4c4a5699f02a327cf0
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2017-10-05 00:56:32 -0400

    Project 4: Index with all articles.

--- projects/project4/index.php
+++ projects/project4/index.php
@@ -10,11 +10,41 @@
             </li>
           </ul>
         </nav>
-        <h3 class="text-muted">Project #[number] "[title]"</h3>
+        <h3 class="text-muted">Project #4 "Designing the Database"</h3>
       </div>
+        <?php
+          include_once("scripts/connect.inc.php");
 
-      [content]
+          $query = "SELECT `title`, `body`, `author`, DATE_FORMAT(`date`, '%M %D, %Y') as 'date' FROM article ORDER BY `date` DESC";
+          $query_run = mysqli_query($mysqli, $query); 
+          
+          while ($query_array = mysqli_fetch_assoc($query_run)) {
+            $title = $query_array['title'];
+            $body = $query_array['body'];
+            $author = $query_array['author'];
+            $date = $query_array['date'];
 
+            $q = "SELECT `firstName`, `lastName` FROM user WHERE `id`=" . $author;
+            $q_r = mysqli_query($mysqli, $q);
+            $q_a = mysqli_fetch_assoc($q_r);
+            $author = $q_a['firstName'] . " " . $q_a['lastName'];
+        ?>
+            <div class="well">
+              <h3><?php echo $title; ?></h3>
+              <hr>
+              <?php echo $body; ?>
+              <hr>
+              <div class="text-muted" style="text-align: right;">
+                <?php echo $author; ?>
+                <br>
+                <?php
+                  echo $date;
+                ?>
+              </div>
+            </div>
+        <?php
+          }
+        ?>
       <footer class="footer">
         <p>&copy; Stephen Floyd <?php echo date("Y"); ?></p>
       </footer>
