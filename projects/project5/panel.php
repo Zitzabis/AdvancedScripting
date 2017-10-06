fa56206d654088316412fc9d18e347a436ec50ac
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2017-10-06 16:51:05 -0400

    Project 5: Adds control panel.

---
+++ projects/project5/panel.php
@@ -0,0 +1,30 @@
+<?php
+  // Author:      Stephen Floyd
+  // Date:        10/6/17
+  // Assignment:  Project #4
+
+  // HTML head file
+  include("../../php/html_head.php");
+?>
+
+  <body>
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills float-right">
+            <li class="nav-item">
+              <a class="nav-link active" href="../../index.php">Index <span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">Project #4 "Designing the Database"</h3>
+      </div>
+        
+        
+      <footer class="footer">
+        <p>&copy; Stephen Floyd <?php echo date("Y"); ?></p>
+      </footer>
+
+    </div> <!-- /container -->
+  </body>
+</html>
