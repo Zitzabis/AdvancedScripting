a6f48050fae8f3475933f74edfd7a7b6159b32dd
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2017-10-28 14:03:10 -0400

    Fixes some styling issues.

--- php/html_head.php
+++ php/html_head.php
@@ -11,8 +11,13 @@
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
     <!-- Custom styles for this template -->
     <link href="../../css/narrow-jumbotron.css" rel="stylesheet">
+
+    
 
     <link rel="stylesheet" href="../../css/fontawesome/css/font-awesome.min.css">
 
