41dc6eca85b41114c6f40559387907bf3f9dc493
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2017-10-28 14:03:45 -0400

    Midterm: Adds varied index pages per login state.

--- projects/midterm/index.php
+++ projects/midterm/index.php
@@ -10,38 +10,24 @@
             </li>
           </ul>
         </nav>
-        <h3 class="text-muted">Project #[number] "[title]"</h3>
+        <h3 class="text-muted">Midterm</h3>
       </div>
+      <?php include("php/login.php"); ?>
+      <br>
+      <br>
       <?php
-        // Check if a user is logged in
-        // If so, display panel button and logout link
-        session_start();
-        if (isset($_SESSION['user_id'])) {
-          if ($_SESSION['teacher'] == "1") {
-            echo '<a href="panel.php"><button type="button" class="btn btn-info">Panel</button></a>';
+        if(isset($_SESSION['teacher'])) {
+          if($_SESSION['teacher'] == 1) {
+            include("php/teacher/index.php");
           }
-          echo '<a href="scripts/logout.php" style="float: right;">Logout</a>';
+          else {
+            include("php/student/index.php");
+          }
         }
-        // Otherwise display login form
         else {
-      ?>
-          <form class="form-inline" action="scripts/checklogin.php">
-            <div class="form-group" style="margin-right: 1em;">
-              Username
-              <input type="username" class="form-control" name="username" placeholder="Username" style="margin-left: 0.5em;">
-            </div>
-            <div class="form-group">
-              Password 
-              <input type="password" class="form-control" name="password" placeholder="Password" style="margin-left: 0.5em;">
-            </div>
-            <button type="submit" class="btn btn-default" style="margin-left: 1em;">Sign in</button>
-          </form>
-      <?php
+          include("php/public/index.php");
         }
       ?>
-      <br>
-      <br>
-      [content]
 
       <footer class="footer">
         <p>&copy; Stephen Floyd <?php echo date("Y"); ?></p>
