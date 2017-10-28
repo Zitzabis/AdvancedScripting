52199350d60cf8638575079e97cc1dae37fccb48
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2017-10-28 14:07:42 -0400

    Midterm: Builds main landing page.

--- projects/midterm/php/public/index.php
+++ projects/midterm/php/public/index.php
@@ -1,8 +1,13 @@
 <div class="panel panel-default">
   <div class="panel-heading">
-    <h3 class="panel-title">Panel title</h3>
+    <h3 class="panel-title">Quizmania</h3>
   </div>
   <div class="panel-body">
-    Panel content
+    Login above to access Quizmania.<br>
+    We offer services for both teachers and students.
+    <h3>Students</h3>
+    Take quizzes assigned to you by your teachers.
+    <h3>Teachers</h3>
+    Create and assign quizzes to your students.
   </div>
 </div>
