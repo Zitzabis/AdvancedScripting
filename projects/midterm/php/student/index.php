406d290e8fbf518ba863e26f86d1b9ff34af07cf
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2017-10-29 01:12:09 -0400

    Midterm: Builds Student taking quiz interface.

---
+++ projects/midterm/php/student/index.php
@@ -0,0 +1,75 @@
+<h2 class="text-muted text-center">Your Quizzes</h2>
+<h3>Todo</h3>
+<table class="table" style="margin-top: 2em;">
+  <thead class="thead-inverse">
+    <tr>
+      <th>Title</th>
+      <th>Take</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+      // Connect to DB
+      include_once("scripts/connect.inc.php");
+
+      $query = 'SELECT quizID FROM user_has_quiz shq WHERE userID=' . $_SESSION['user_id'] . ' AND completed=0 ORDER BY id ASC';
+      $query_run = mysqli_query($mysqli, $query); 
+      
+      // Tick through all results from the query
+      while ($query_array = mysqli_fetch_assoc($query_run)) {
+        // Fetch columns and store into vars
+        $quizID = $query_array['quizID'];
+
+        $q = "SELECT title FROM quiz WHERE id=" . $quizID;
+        $qr = mysqli_query($mysqli, $q);
+        $qa = mysqli_fetch_assoc($qr);
+        $title = $qa['title'];
+
+        // Check if the row needs to be marked as deleted
+        echo "<tr>";
+          // Fill out table with data
+          echo "<td>" . $title . "</td>";
+          echo '<td><a href="take_quiz.php?id=' . $quizID . '"><button type="button" class="btn btn-primary">Take Quiz</button></a></td>';
+        echo "</tr>";
+      }
+    ?>
+  </tbody>
+</table>
+
+<h3>Completed</h3>
+<table class="table" style="margin-top: 2em;">
+  <thead class="thead-inverse">
+    <tr>
+      <th>Title</th>
+      <th>Grade</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+      // Connect to DB
+      include_once("scripts/connect.inc.php");
+
+      $query = 'SELECT quizID, score FROM user_has_quiz WHERE userID=' . $_SESSION['user_id'] . ' AND completed=1 ORDER BY id ASC';
+      $query_run = mysqli_query($mysqli, $query); 
+      
+      // Tick through all results from the query
+      while ($query_array = mysqli_fetch_assoc($query_run)) {
+        // Fetch columns and store into vars
+        $quizID = $query_array['quizID'];
+        $grade = $query_array['score'];
+
+        $q = "SELECT title FROM quiz WHERE id=" . $quizID;
+        $qr = mysqli_query($mysqli, $q);
+        $qa = mysqli_fetch_assoc($qr);
+        $title = $qa['title'];
+
+        // Check if the row needs to be marked as deleted
+        echo "<tr>";
+          // Fill out table with data
+          echo "<td>" . $title . "</td>";
+          echo '<td>' . $grade . '</td>';
+        echo "</tr>";
+      }
+    ?>
+  </tbody>
+</table>
---
+++ projects/midterm/take_quiz.php
@@ -0,0 +1,64 @@
+<?php include("../../php/html_head.php") ?>
+
+  <body>
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills float-right">
+            <li class="nav-item">
+              <a class="nav-link active" href="../../index.php">Index <span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">Midterm</h3>
+      </div>
+      <?php include("php/login.php"); ?>
+      <br>
+      <br>
+      <form class="form" action="scripts/quiz_submit.php">
+        <?php
+            // Connect to DB
+            include_once("scripts/connect.inc.php");
+
+            $quiz = $_GET['id'];
+
+            $query = 'SELECT title FROM quiz WHERE id=' . $quiz;
+            $query_run = mysqli_query($mysqli, $query);
+            $query_array = mysqli_fetch_assoc($query_run);
+            $title = $query_array['title'];
+
+            $query = 'SELECT questionID, question, options, points FROM question WHERE quiz=' . $quiz;
+            $query_run = mysqli_query($mysqli, $query);
+            
+            // Tick through all results from the query
+            while ($query_array = mysqli_fetch_assoc($query_run)) {
+                // Fetch columns and store into vars
+                $questionID = $query_array['questionID'];
+                $question = $query_array['question'];
+                $options = $query_array['options'];
+                $points = $query_array['points'];
+
+                $optionsArray = explode("\n", $options);
+
+                echo '<h1 class="text-center">' . $title . '</h1>';
+                echo '<h3 class="text-muted">' . $question . '</h3>';
+
+                foreach ($optionsArray as $value) {
+                    echo '<input type="radio" name="question' . $questionID . '" value="' . $value . '"> ' . $value . '<br>';
+                }
+                echo "<hr>";
+            }
+        ?>
+        <div class="text-center"><button type="submit" class="btn btn-success">Submit Quiz</button></div>
+        <br>
+        <br>
+      </form>
+      <footer class="footer">
+        <p>&copy; Stephen Floyd <?php echo date("Y"); ?></p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <?php include("../../php/javascript.php") ?>
+  </body>
+</html>
