daa021a72aa027267b2c05023f8661770268a473
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2017-10-30 01:21:10 -0400

    Directs all connection files to one location.

--- projects/midterm/take_quiz.php
+++ projects/midterm/take_quiz.php
@@ -3,7 +3,7 @@
   // Date:        10/30/17
   // Assignment:  Midterm
 
-  include("../../php/html_head.php")
+  include("../../php/html_head.php");
 
   if (session_status() == PHP_SESSION_NONE) {
     session_start();
